b84d609de3181b09a4b008b2a3625e9b3a3ef2d0
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-19 19:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  if ( timeString < 12){
+    return "Good Morning";
+  }
+  if ( timeString > 12){
+    return "Good Morning";
+  }
+  if ( timeString > 12){
+    return "Good Morning";
+  }
+}
 /* Write your implementation of displayMessage() */
